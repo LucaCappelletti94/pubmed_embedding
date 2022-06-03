8e93e1d4d472bd961dbd327d308c263c1f0a5e02
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-06-03 12:26:08 +0200

    Testing on full pubmed 2022

--- pubmed_parser/tests/test_parse_pubmed.rs
+++ pubmed_parser/tests/test_parse_pubmed.rs
@@ -2,6 +2,6 @@ extern crate pubmed_parser;
 
 #[test]
 fn test_pubmed_parser() {
-    let result = pubmed_parser::parse_pubmed("../data/");
+    let result = pubmed_parser::parse_pubmed("/bfd/pubmed/");
     assert!(result.is_ok());
 }
